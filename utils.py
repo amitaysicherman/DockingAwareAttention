6a3f5903e6d09e736fcad5eb0425fc50e0bf800f
amitay.s <amitay.sich@gmail.com>
2024-12-17 09:55:02 +0200

    start

--- utils.py
+++ utils.py
@@ -28,8 +28,10 @@ class ProteinsManager:
     def get_id(self, ec):
         if "[" in ec:  # tokenized
             ec = ec.replace("]", ".").replace("[", "").replace("v", "").replace("u", "").replace("t", "").replace("q","")
+        print(ec)
         if ec not in self.ec_to_id:
             return None
+
         return self.ec_to_id[ec]
 
     def get_base_dir(self, id_):
