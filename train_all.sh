28f5124913092d25ba1e9353856ce44bd26165d9
amitay.s <amitay.sich@gmail.com>
2024-12-17 21:58:04 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -2,19 +2,19 @@
 #SBATCH --time=7-00
 #SBATCH --mem=256G
 #SBATCH --requeue
-#SBATCH --gres=gpu:L40:1
+#SBATCH --gres=gpu:L40:2
 #SBATCH --array=1-10
 
-configs="--ec_type 0|\
-          --ec_type 1|\
-          --ec_type 2 --daa_type 1|\
-          --ec_type 2 --daa_type 2|\
-          --ec_type 2 --daa_type 3|\
-          --ec_type 2 --daa_type 4|\
-          --ec_type 2 --daa_type 1 --add_ec_tokens 1|\
-          --ec_type 2 --daa_type 2 --add_ec_tokens 1|\
-          --ec_type 2 --daa_type 3 --add_ec_tokens 1|\
-          --ec_type 2 --daa_type 4 --add_ec_tokens 1"
+configs="--ec_type 0 --batch_size_factor 1|\
+          --ec_type 1 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 1 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 2 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 3 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 4 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 1 --add_ec_tokens 1 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 2 --add_ec_tokens 1 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 3 --add_ec_tokens 1 --batch_size_factor 1|\
+          --ec_type 2 --daa_type 4 --add_ec_tokens 1 --batch_size_factor 1"
 
 # Split the config string into an array using '|' as a delimiter
 IFS='|' read -ra config_array <<< "$configs"
