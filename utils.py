ae96719d2c4b75c8550afc72c0eb34ffe2c7f664
amitay.s <amitay.sich@gmail.com>
2024-12-17 09:56:50 +0200

    start

--- utils.py
+++ utils.py
@@ -27,8 +27,8 @@ class ProteinsManager:
 
     def get_id(self, ec):
         if "[" in ec:  # tokenized
-            ec = ec.replace("]", ".").replace("[", "").replace("v", "").replace("u", "").replace("t", "").replace("q","")
-        print(ec)
+            ec = ec.replace("v", "").replace("u", "").replace("t", "").replace("q", "").replace("[", "")
+            ec = ".".join(ec.split("]"))
         if ec not in self.ec_to_id:
             return None
 
