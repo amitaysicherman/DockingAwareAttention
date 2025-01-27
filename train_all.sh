4ab2d38befe2b232b1613970da1f2112aec650ed
amitay.s <amitay.sich@gmail.com>
2025-01-27 13:50:23 +0200

    start

--- train_all.sh
+++ train_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --requeue
 #SBATCH -c 8
 #SBATCH --gres=gpu:L40:1
-#SBATCH --array=1-3
+#SBATCH --array=1-2
 
 #MASTER_PORT=$((29500 + SLURM_ARRAY_TASK_ID))
 #echo "MASTER_PORT: $MASTER_PORT"
@@ -19,8 +19,8 @@
 #     --ec_type 2 --daa_type 2 --add_ec_tokens 1|\
 #     --ec_type 2 --daa_type 3 --add_ec_tokens 1|\
 #     --ec_type 2 --daa_type 4 --add_ec_tokens 1"
-configs=" --ec_type 2 --daa_type 1 --add_ec_tokens 1 --emb_suf _pb|\
- --ec_type 2 --daa_type 1 --add_ec_tokens 1 --emb_suf _gn|\
+configs=" --ec_type 2 --daa_type 1 --add_ec_tokens 1 --emb_suf _pb1|\
+ --ec_type 2 --daa_type 1 --add_ec_tokens 1 --emb_suf _gn1|\
   --ec_type 2 --daa_type 1 --add_ec_tokens 1 --emb_suf _re"
 
 # Split the config string into an array using '|' as a delimiter
